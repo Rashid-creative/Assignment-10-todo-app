d83c6c37e2a0bedd9761b9dccfc03e8ac7783c4b
Rashid Mahmood <insprashid67@gmail.com>
2023-08-01 09:14:10 +0500

    This is my 3rd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
-function App() {
+const App = () => {
+  const initialTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  const [todos, setTodos] = useState(initialTodos);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === "") return;
+
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Date.now(), text: newTodo, completed: false },
+    ]);
+    setNewTodo("");
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleCompleteTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Todo App</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Enter your todo"
+          value={newTodo}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <TodoList
+        todos={todos}
+        onDelete={handleDeleteTodo}
+        onComplete={handleCompleteTodo}
+      />
     </div>
   );
-}
+};
 
 export default App;
